7bd5dbd9313a6a04b68a05cae03b96f482de5270
LPCS69 <lucespedes@estudiantec.cr>
2024-10-10 17:53:41 -0600

    Create appServer.py

---
+++ appServer.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'hello world from /'
+
+@app.route('/path1')
+def hello_path1():
+    return 'hello world from /path1'
+
+@app.route('/path1/subpath1')
+def hello_subpath1():
+    return 'hello world from /path1/subpath1'
+
+if __name__ == '__main__':
+    app.run(debug=True)
