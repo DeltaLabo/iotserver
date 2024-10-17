276c8b30714568b70e6eb496316b54e5dafd2393
LPCS69 <lucespedes@estudiantec.cr>
2024-10-17 14:40:48 -0600

    gitTest

--- appServer.py
+++ appServer.py
@@ -4,15 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'hello world from /'
+    return 'hello universe from /'
 
 @app.route('/path1')
 def hello_path1():
-    return 'hello world from /path1'
+    return 'hello universe from /path1'
 
 @app.route('/path1/subpath1')
 def hello_subpath1():
-    return 'hello world from /path1/subpath1'
+    return 'hello universe from /path1/subpath1'
 
 if __name__ == '__main__':
     app.run(debug=True)
